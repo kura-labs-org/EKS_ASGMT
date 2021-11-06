14140d64aacc308fabf6a9e9fd0f290ad656e93a
JespsonSTP <jespstpierre@yahoo.com>
2021-11-05 22:47:57 -0400

    setting up event

--- nats-service/src/listener.ts
+++ nats-service/src/listener.ts
@@ -15,76 +15,32 @@ stan.on('connect', () =>{
         process.exit();
     })
 
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('the service');
+    new UserCreatedListener(stan).listen();
+    new ChefCreatedListener(stan).listen();
 
-    const subscription = stan.subscribe(
-        'store:created',
-        'store-created-queue-group',
-        options);
-
-    subscription.on('message', (msg: Message) =>{
-        console.log('message received')
-        const data = msg.getData();
-
-        if (typeof data === 'string') {
-            console.log(`ths event #${msg.getSequence()} and the data is ${data}`)
-        }
-
-        msg.ack();
-    })
 })
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
 
-abstract class Listener {
-    abstract subject: string;
-    abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void;
-    private client: Stan;
-    private ackWait = 5 * 1000;
+class UserCreatedListener extends Listener {
+    subject = 'user:created';
+    queueGroupName = 'payments-service';
 
-    constructor(client: Stan) {
-        this.client = client;
-    }
+    onMessage(data: any, msg: Message) {
+        console.log('event created', data);
 
-    subscriptionOption() {
-        return this.client
-            .subscriptionOptions()
-            .setDeliverAllAvailable()
-            .setManualAckMode(true)
-            .setAckWait(this.ackWait)
-            .setDurableName(this.queueGroupName);
-    };
-
-    listen() {
-        const subscription = this.client.subscribe(
-            this.subject,
-            this.queueGroupName,
-            this.subscriptionOption()
-        );
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message received: ${this.subject} / ${this.queueGroupName}`
-            );
-
-            const parsedData = this.parseMessage(msg);
-            this.onMessage(parsedData, msg);
-        });
-    }
-
-    parseMessage(msg: Message) {
-        const data = msg.getData()
-
-        return typeof data === 'string'
-            ? JSON.parse(data)
-            : JSON.parse(data.toString('utf-8'));
+        msg.ack();
     }
 }
+class ChefCreatedListener extends Listener{
+    subject = 'chef:created';
+    queueGroupName = 'payments-service';
 
+    onMessage(data: any, msg: Message) {
+        console.log('Even Created', data);
+
+        msg.ack();
+    }
+}
 
